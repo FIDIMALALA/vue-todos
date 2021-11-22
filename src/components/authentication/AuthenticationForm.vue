82940137c86152ca6e6af4bb17ef1d09f1c2e7a6
FIDIMALALA <ismael.fidimalala@gmail.com>
2021-11-22 22:01:14 +0300

    Initial commit

---
+++ src/components/authentication/AuthenticationForm.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Authentication Form works</h1>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class AuthenticationForm extends Vue {}
+</script>
---
+++ src/views/Authentication.vue
@@ -0,0 +1,13 @@
+<template>
+  <authentication-form />
+</template>
+
+<script lang="ts">
+import AuthenticationForm from '@/components/authentication/AuthenticationForm.vue'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: { AuthenticationForm }
+})
+export default class Authentication extends Vue {}
+</script>
